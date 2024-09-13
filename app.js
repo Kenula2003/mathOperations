70c7011c88d8f4b49d5459a50f52995cb8444839
Kenula2003 <75131621+Kenula2003@users.noreply.github.com>
2024-09-13 17:55:54 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+const addNumbers = require('./mathOperations');
+
+const num1 = 5;
+const num2 = 6;
+const result = addNumbers(num1, num2);
+
+console.log(`The sum of ${num1} and ${num2} is: ${result}`);
